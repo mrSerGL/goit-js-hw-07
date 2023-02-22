5538e737c212e0ef45153ef12c289c7aad9eaae6
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-02-22 11:04:27 +0200

    current

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+console.log(galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}" target="_blank>
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="large-image.jpg"
+            alt="${description}"
+          />
+        </a>
+      </div>
+      `;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
